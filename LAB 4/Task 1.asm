249c968227fa182e408ae0efcbb7bad7fca2b817
Tanmoy Saha <tanmoysaha2003@gmail.com>
2025-04-16 00:40:48 +0600

    Task 1

---
+++ LAB 4/Task 1.asm
@@ -0,0 +1,34 @@
+.MODEL SMALL
+
+.STACK 100H
+
+.DATA
+    STAR DB '*'   ; ASCII character for asterisk
+
+.CODE
+MAIN PROC
+
+    ; initialize DS
+    MOV AX, @DATA
+    MOV DS, AX
+
+    ; set counter for loop (80 times)
+    MOV CX, 80
+
+    ; set the character to display
+    MOV DL, STAR
+
+PRINT_STAR:
+    ; output the character in DL
+    MOV AH, 02H
+    INT 21H
+
+    ; loop 80 times
+    LOOP PRINT_STAR
+
+    ; exit to DOS
+    MOV AX, 4C00H
+    INT 21H
+
+MAIN ENDP
+END MAIN
